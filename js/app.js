c328d9461f819511fd409b5b33522c2830e262ee
werbnah <andrewchau1993@gmail.com>
2024-07-18 09:47:20 -0700

    removed all comments

--- js/app.js
+++ js/app.js
@@ -27,12 +27,9 @@ document.addEventListener("keydown", handleKeydown);
 resetButton.addEventListener("click", resetGame);
 helpButton.addEventListener("click", toggleHelp);
 /*-------------------------------- Functions --------------------------------*/
-//the init function should create the board.
 function init() {
-  //function to make the creation of the board.
   board.innerHTML = "";
   for (let i = 0; i < BOARD_SIZE * BOARD_SIZE; i++) {
-    //this line makes it so they are gridded together vs being put into one little cube over and over again.
     let cell = document.createElement("div");
     cell.classList.add("cell");
     board.appendChild(cell);
@@ -41,9 +38,7 @@ function init() {
   placeFruit();
 }
 function updateBoard() {
-  // snake and food creation
   let cells = document.querySelectorAll(".cell");
-  // remove classes from all cells
   cells.forEach((cell) =>
     cell.classList.remove(
       "snake-head",
@@ -56,8 +51,6 @@ function updateBoard() {
       "ArrowRight"
     )
   );
-
-  // add classes to snake segments
   snake.forEach((segment, index) => {
     let cellIndex = segment.y * BOARD_SIZE + segment.x;
     cells[cellIndex].classList.add("snake");
@@ -67,42 +60,18 @@ function updateBoard() {
       cells[cellIndex].classList.add("snake-tail");
     }
   });
-  // add class to fruit
   let fruitIndex = fruit.y * BOARD_SIZE + fruit.x;
   cells[fruitIndex].classList.add("fruit");
 }
 function placeFruit() {
-  //function to move food.
   fruit = {
     x: Math.floor(Math.random() * BOARD_SIZE),
     y: Math.floor(Math.random() * BOARD_SIZE),
   };
-  //its makes it so it replaces fruit if it lands on the snake...
   if (snake.some((segment) => segment.x === fruit.x && segment.y === fruit.y)) {
     placeFruit();
   }
 }
-
-//implements game start upon keypress.
-function handleKeydown(event) {
-  if (!gameStarted && DIRECTIONS[event.key]) {
-    gameStarted = true;
-    gameInterval = setInterval(moveSnake, 100);
-  }
-  //deny the snake of moving backwards.
-  if (DIRECTIONS[event.key] && !directionChanged && !gameOver) {
-    let newDirection = DIRECTIONS[event.key];
-    let currentDirection = DIRECTIONS[direction];
-    if (
-      newDirection.x !== -currentDirection.x &&
-      newDirection.y !== -currentDirection.y
-    ) {
-      direction = event.key; // update direction to the key pressed
-      directionChanged = true;
-    }
-  }
-}
-//deny the snake of moving backwards, this code allows for 1 change of direction per square
 function moveSnake() {
   let head = {
     x: snake[0].x + DIRECTIONS[direction].x,
@@ -120,7 +89,6 @@ function moveSnake() {
     message.textContent = "Git Good!";
     return;
   }
-  //unshift method to grow the snake.
   snake.unshift(head);
   if (head.x === fruit.x && head.y === fruit.y) {
     fruitCount++;
@@ -139,22 +107,37 @@ function moveSnake() {
   updateBoard();
   directionChanged = false;
 }
-//reset function to reset everything back to base functionality of the game.
+function handleKeydown(event) {
+  if (!gameStarted && DIRECTIONS[event.key]) {
+    gameStarted = true;
+    gameInterval = setInterval(moveSnake, 100);
+  }
+  if (DIRECTIONS[event.key] && !directionChanged && !gameOver) {
+    let newDirection = DIRECTIONS[event.key];
+    let currentDirection = DIRECTIONS[direction];
+    if (
+      newDirection.x !== -currentDirection.x &&
+      newDirection.y !== -currentDirection.y
+    ) {
+      direction = event.key; 
+      directionChanged = true;
+    }
+  }
+}
 function resetGame() {
   clearInterval(gameInterval);
   snake = [{ x: 7, y: 12 }];
-  direction = "ArrowUp"; // Reset direction
+  direction = "ArrowUp";
   fruit = { x: 7, y: 7 };
   fruitCount = 0;
   fruitCountDisplay.textContent = `I'm just a baby`;
   gameOver = false;
-  gameStarted = false; //reset game status
-  directionChanged = false; //resets direction
+  gameStarted = false;
+  directionChanged = false;
   message.textContent = "Get Ready!";
   init();
   updateBoard();
 }
-//this toggles the help button
 function toggleHelp() {
   if (helpMessage.style.display === "none") {
     helpMessage.style.display = "block";
